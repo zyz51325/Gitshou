84c8ce16ddab4ca197b7d303fea03985afade2d3
zyz51325 <1287318179@qq.com>
2019-02-19 16:46:05 +0800

    yiqi

--- Gitshow/Program.cs
+++ Gitshow/Program.cs
@@ -9,6 +9,8 @@ namespace Gitshow
             Console.WriteLine("Hello World!");
             //改一点吧哈哈
             //飞哥搞破坏
+            Console.WriteLine("飞哥真\"帅\"");
+            Console.Read();
         }
     }
 }
