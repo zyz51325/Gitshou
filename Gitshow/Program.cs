12728c81899affb0f45182c244d6825dc58aa514
zyz51325 <1287318179@qq.com>
2019-02-21 00:06:38 +0800

    用字符串求最大值

--- Gitshow/Program.cs
+++ Gitshow/Program.cs
@@ -6,21 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = new[] { 299, 11, 2334, 555 };
+            int[] array = new[] { 29, 11, 34, 55, 99, 65 };
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
             }
-            int max = array[0] ;
+            int max = array[0];
+
             for (int i = 1; i < array.Length; i++)
             {
                 if (array[i] > max)
 
                 {
+                    Console.WriteLine($"第{i}次比较:{array[i]}>{max},最大值为{array[i]}");
                     max = array[i];
                 }
+                else
+                {
+                    Console.WriteLine($"第{i}次比较:{array[i]}<={max},最大值为{max}");
+                }
             }
-            Console.WriteLine("最大值:"+max);
+            Console.WriteLine("因此最大值为" + max);
 
             Console.Read();
         }
