125ab95fa2a8d75b209524555f9e2e7eb0eec3d9
zyz51325 <1287318179@qq.com>
2019-02-21 11:58:27 +0800

    猜数值

--- Gitshow/Program.cs
+++ Gitshow/Program.cs
@@ -6,29 +6,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 29, 11, 34, 55, 99, 65 };
+            //int[] array = new int[] { 29, 11, 34, 55, 99, 65 };
             //for (int i = 0; i < array.Length; i++)
             //{
             //    Console.WriteLine(array[i]);
             //}
-            int max = array[0];
+            //int max = array[0];
 
-            for (int i = 1; i < array.Length; i++)
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    if (array[i] > max)
+
+            //    {
+            //        Console.WriteLine($"第{i}次比较:{array[i]}>{max},最大值为{array[i]}");
+            //        max = array[i];
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"第{i}次比较:{array[i]}<={max},最大值为{max}");
+            //    }
+            //}
+            //Console.WriteLine("因此最大值为" + max);
+            int random =new Random().Next(1000);
+            int input = 0;
+            for (int i = 1; i < 10; i++)
             {
-                if (array[i] > max)
 
+                Console.WriteLine("请输入一个数:");
+
+                string strInput = Console.ReadLine();
+                input = Convert.ToInt32(strInput);
+
+                if (input == random)
                 {
-                    Console.WriteLine($"第{i}次比较:{array[i]}>{max},最大值为{array[i]}");
-                    max = array[i];
+                    Console.WriteLine("答对了，就是正确答案");
+                    
+
+                }
+                else if (input < random)
+                {
+                    Console.WriteLine($"第{i}次比较:数值小了");
                 }
                 else
                 {
-                    Console.WriteLine($"第{i}次比较:{array[i]}<={max},最大值为{max}");
+                    Console.WriteLine($"第{i}次比较:数值大了");
                 }
             }
-            Console.WriteLine("因此最大值为" + max);
-
-            Console.Read();
+                Console.ReadLine();
+            
+       
         }
     }
 }
