130e1d63f45777eecd89a7dcc95ba95914ad5368
zyz51325 <1287318179@qq.com>
2019-02-28 23:44:12 +0800

    一年所有周

--- Gitshow/Program.cs
+++ Gitshow/Program.cs
@@ -1,78 +1,145 @@
 ﻿using System;
-using CQ = China.Chongqing;
-using TW = China.Taiwan;
+//using CQ = China.Chongqing;
+//using TW = China.Taiwan;                      //引用类型放在堆里面，值类型有可能在堆里有可能在栈里
 
 namespace China.meinv
 {
     class Program
     {
 
-        static void Main(string[] args)   //方法与main并列
+
+        static void Main(string[] args)
         {
 
-            CQ.Beauty 蒋勤勤 = new CQ.Beauty(168, 55, "蒋勤勤", true);
-            Console.WriteLine($"重庆美女{蒋勤勤.Name} 身高{蒋勤勤.Height}cm，体重{蒋勤勤.Weight}公斤，是否婚配：{蒋勤勤.Married}");//按.后自动显示参数
+            DateTime today = new DateTime(2019, 1, 1);
+            Console.WriteLine("2019年1月1日为" + today.DayOfWeek.ToString() + ",");
 
-            TW.Beauty 林志玲 = new TW.Beauty(166, 44, "林志玲", false);
-            Console.WriteLine($"台湾美女{林志玲.Name} 身高{林志玲.Height}cm，体重{林志玲.Weight}公斤，是否婚配：{林志玲.Married}");
-                                       //字段.方法
+            for (int i = 0; i < 7; i++)
+            {
+                if (today.DayOfWeek.ToString() == DayOfWeek.Sunday.ToString())
+                {
+                    break;
+                }
+                else
+                {
+                    today = today.AddDays(1);
+                }
+            }
+            Console.WriteLine($"{today.Year}年{today.Month}月{today.Day}日为该年第一个星期天。");
+            for (int i = 0; i < (365 + 7) / 7; i++)
+            {
+                DateTime firstday = today.AddDays(7 * i - 7);
+                Console.Write($"第{i + 1}周:{firstday.Year}年{firstday.Month}月{firstday.Day}日");
+                DateTime lastday = today.AddDays(7 * i - 1);
+                Console.WriteLine($"------{lastday.Year}年{lastday.Month}月{lastday.Day}日");
+
+            }
             Console.Read();
         }
     }
 }
 
-namespace China.Chongqing
-{
-    struct Beauty //定义一个美女
-    {
-        public int Height;//定义身高   自己写的代码：总是给最低权限
-        public int Weight;
-        public string Name;
-        internal bool Married;
-        public static string Gender = "Female";
-        public Beauty(int weight, int height, string name, bool married)//参数
-        {
-            Height = height;
-            Weight = weight;
-            Name = name;
-            Married = married;
-        }
-
-    }
-}
 
 
-namespace China.Taiwan
-{
-    struct Beauty
-    {
-        public int Height;
-        public int Weight;
-        public string Name;
-        internal bool Married;
-        public Beauty(int height, int weight, string name, bool married)
-        {
-            Height = height;
-            Weight = weight;
-            Name = name;
-            Married = married;
-        }
-
-    }
-}
+//值类型的值传递
+//static void Main(string[] args)     
+//{
+//    int i = 5;
+//    Show(i);
+//    Console.WriteLine("i=" + i);
+//    Console.Read();
+//}
+//static void Show(int i)
+//{
+//    i++;
+//}
 
 
+//        值类型的引用传递
+//    static void Main(string[] args)
+//    {
+//        int i = 5;
+//    Show(ref i);
+//    Console.WriteLine("i=" + i);
+//        Console.Read();
+//    }
+//static int Show(ref int i)
+//{
+//    i++;
+//    return i;
+//}
 
 
+//        static void Main(string[] args)
 
-//            //求最大值
-//            int[] array = new int[] { 1, 2, 3, 8, 677, 7, 5, 4 };
-//            Console.WriteLine("数组为:1,2,3,8,677,7,5,4。");
-//            Shou(array);
+//        {
+//            CQ.Beauty 蒋勤勤 = new CQ.Beauty(168, 55, "蒋勤勤", true);
+//            Console.WriteLine($"重庆美女{蒋勤勤.Name} 身高{蒋勤勤.Height}cm，体重{蒋勤勤.Weight}公斤，是否婚配：{蒋勤勤.Married}");//按.后自动显示参数
+
+//            TW.Beauty 林志玲 = new TW.Beauty(166, 44, "林志玲", false);
+//            Console.WriteLine($"台湾美女{林志玲.Name} 身高{林志玲.Height}cm，体重{林志玲.Weight}公斤，是否婚配：{林志玲.Married}");
 
 //            Console.Read();
+
 //        }
-//        static int Shou(int[] array)
+//    }
+//}
+
+//namespace China.Chongqing   //using下调用，如果重庆和台湾同时删除，则无法识别哪个地方的beauty
+//{
+//    struct Beauty //定义一个美女 //以上两段即为CQ.Beauty
+//    {
+//        public int Height;//定义身高   自己写的代码：总是给最低权限
+//        internal int Weight;
+//        public string Name;
+//        internal bool Married;
+
+//        //public static string Gender = "Female";
+//        public Beauty(int height, int weight, string name, bool married)//参数
+//        {
+//            Height = height;
+//            Weight = weight;
+//            Name = name;
+//            Married = married;
+//        }
+
+//    }
+//}
+
+
+//namespace China.Taiwan
+//{
+//    struct Beauty
+//    {
+//        public int Height;
+//        public int Weight;
+//        public string Name;
+//        internal bool Married;
+//        public Beauty(int height, int weight, string name, bool married)
+//        {
+//            Height = height;
+//            Weight = weight;
+//            Name = name;
+//            Married = married;
+//        }
+
+//    }
+//}
+
+
+
+
+
+
+////求最大值
+//        static void Main(string[] args)
+//        {
+//            int[] array = new int[] { 1, 2, 3, 8, 677, 7, 5, 4 };
+//            Console.WriteLine("数组为:1,2,3,8,677,7,5,4。");
+//            Show(array);
+//            Console.Read();
+//        }
+//        static int Show(int[] array)
 //        {
 //            int max = array[0];
 //            for (int i = 1; i < array.Length; i++)
@@ -98,12 +165,12 @@ namespace China.Taiwan
 
 
 
-
+//        static void Main(string[] args)
+//        {
 //            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
 
 //            Console.WriteLine("要查找的数：");
 //            Show(array);
-
 //            Console.WriteLine();
 //            Console.Read();
 //        }
@@ -143,21 +210,22 @@ namespace China.Taiwan
 
 
 
-//随机数组
+//        随机数组
+//        static void Main(string[] args)
+//        {
+//            int length = 10;
 
-//        int length = 10;
-
-//        int[] array = new int[length];   //方括号为长度，大括号为具体数组
-//        int i = 0;
-//        array[0] = 2;
+//            int[] array = new int[length];   //方括号为长度，大括号为具体数组
+//            int i = 0;
+//            array[0] = 2;
 //            Console.Write("您要的数组为:" + array[0] + ",");
-//            Show(array,i,length );
+//            Show(array, i, length);
 
-//        array[length - 1] = array[length - 2] + new Random().Next(3, 16);
+//            array[length - 1] = array[length - 2] + new Random().Next(3, 16);
 //            Console.Write(array[length - 1] + "。");
 //            Console.Read();
 //        }
-//        static int Show(int[] array,int i,int length)
+//        static int Show(int[] array, int i, int length)
 //        {
 
 
@@ -175,15 +243,17 @@ namespace China.Taiwan
 
 
 
-//数值交换
 
+//        数值交换
 
+//        static void Main(string[] args)
+//        {
 //            float a = 3.7f;
 //            float b = 5.2f;
 //            Swap(ref a, ref b);              //ref 参数传递                                  //Show（array）;
 //            Console.WriteLine("结果为:");
 //            Console.WriteLine("a=" + a);                                              //Show(int[] array);
-//            Console.WriteLine("b=" + b);          
+//            Console.WriteLine("b=" + b);
 //            Console.Read();
 //        }
 //        static float Swap(ref float a, ref float b)
@@ -200,7 +270,8 @@ namespace China.Taiwan
 //}
 
 
-
+//        static void Main(string[] args)
+//        {
 //            bool result;
 //            Console.WriteLine("输入一个数：");
 //            int i = Convert.ToInt32(Console.ReadLine());//输入一个数，转化为电脑可读的int类型
@@ -229,10 +300,12 @@ namespace China.Taiwan
 
 
 //数值相加
+//        static void Main(string[] args)
+//        {
 //            int a = 3;
 //            int b = 5;
 //            int t = Add(a, b);
-//            Console.WriteLine("相加结果为:t="+t);
+//            Console.WriteLine("相加结果为:t=" + t);
 //            Console.Read();
 //        }
 //        static int Add(int a, int b)
