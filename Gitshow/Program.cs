6a06192caac9df4e2ef525a734f9dad47843bde4
zyz51325 <1287318179@qq.com>
2019-03-12 18:08:19 +0800

    html css

--- Gitshow/Program.cs
+++ Gitshow/Program.cs
@@ -7,9 +7,22 @@ namespace home
 {
     class Program
     {
-        //static void Main(string[] args)
+        static void Main(string[] args)
 
-
+        //空间数组
+        {
+            int[,] multiplearray = new int[4, 5];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 5; j++)
+                {
+                    multiplearray[i, j] = i + j;
+                    Console.Write(multiplearray[i, j] + "   ");
+                }
+            }
+            Console.Read();
+        }
 
 
 
@@ -54,20 +67,7 @@ namespace home
 
 
 
-        //空间数组
-        //{
-        //    int[,] multiplearray = new int[4, 5];
-        //    for (int i = 0; i < 4; i++)
-        //    {
-        //        Console.WriteLine();
-        //        for (int j = 0; j < 5; j++)
-        //        {
-        //            multiplearray[i, j] = i + j;
-        //            Console.Write(multiplearray[i, j] + "   ");
-        //        }
-        //    }
-        //    Console.Read();
-        //}
+
 
 
 
